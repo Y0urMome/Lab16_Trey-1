4956795fb156890a735f5e58fcf8a7e9933deb22
Y0urMome <treyblue14@gmail.com>
2025-05-04 11:44:31 -0400

    Finished tutorial for CSV files

--- sitka_temps.py
+++ sitka_temps.py
@@ -3,7 +3,7 @@ import csv
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-path = Path('sitka_weather_07-2018_simple.csv')
+path = Path('sitka_weather_2018_full.csv')
 lines = path.read_text(encoding='utf-8').splitlines()
 
 reader = csv.reader(lines)
@@ -20,16 +20,28 @@ high_temps = []
 low_temps = []
 
 for row in reader:
-    high = int(row[5])
-    low = int(row[6])
-    high_temps.append(high)
-    low_temps.append(low)
+    try:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
+        high = int(row[8])
+        low = int(row[9])
+    except ValueError as e:
+        print(current_date)
+    else:
+        dates.append(current_date)
+        high_temps.append(high)
+        low_temps.append(low)
 
 # Graphing processed info
 
 plt.style.use('dark_background')
 figure, graph = plt.subplots()
 
-graph.plot(high_temps, color='red')
+graph.plot(dates, high_temps, color='red')
+graph.plot(dates, low_temps, color='blue')
+graph.fill_between(dates, high_temps, low_temps, color='lightblue', alpha=0.5)
+
+graph.set_title('Daily Temps for 2018 in Sitka, Alaska', fontsize=24)
+graph.set_ylabel('Temperature (F)', fontsize=16)
+figure.autofmt_xdate()
 
 plt.show()
